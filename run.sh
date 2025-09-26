4b934a232356ed6470c55563a4b1e89416ae206e
loo <loofxtrt@gmail.com>
2025-09-26 11:48:59 -0300

    cleanup, custom svg, adjusments

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/bash
 
-#target="/mnt/seagate/obsidian-vaults/plugin-development/.obsidian/plugins/file-glyphs"
+target0="/mnt/seagate/obsidian-vaults/plugin-development/.obsidian/plugins/file-glyphs"
 target="/mnt/seagate/obsidian-vaults/projects/.obsidian/plugins/file-glyphs"
 target2="/mnt/seagate/obsidian-vaults/central/.obsidian/plugins/file-glyphs"
 
 # compila o js e copia os arquivos necessários pro plugin funcionar
 npm run build
+
+rm -rf $target0
+mkdir -p $target0
 
 rm -rf $target
 mkdir -p $target
@@ -13,6 +16,7 @@ mkdir -p $target
 rm -rf $target2
 mkdir -p $target2
 
+cp manifest.json main.js styles.css $target0
 cp manifest.json main.js styles.css $target
 cp manifest.json main.js styles.css $target2
 
