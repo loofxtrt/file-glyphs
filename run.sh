9f87f9cd545002952ed79af247ef44bd5cdbb3bc
loo <loofxtrt@gmail.com>
2025-09-28 21:02:41 -0300

    etc

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 target0="/mnt/seagate/obsidian-vaults/plugin-development/.obsidian/plugins/file-glyphs"
 target="/mnt/seagate/obsidian-vaults/projects/.obsidian/plugins/file-glyphs"
 target2="/mnt/seagate/obsidian-vaults/central/.obsidian/plugins/file-glyphs"
+target3="/mnt/seagate/obsidian-vaults/restricted/.obsidian/plugins/file-glyphs"
 
 # compila o js e copia os arquivos necessários pro plugin funcionar
 npm run build
@@ -16,9 +17,13 @@ mkdir -p $target
 rm -rf $target2
 mkdir -p $target2
 
+rm -rf $target3
+mkdir -p $target3
+
 cp manifest.json main.js styles.css $target0
 cp manifest.json main.js styles.css $target
 cp manifest.json main.js styles.css $target2
+cp manifest.json main.js styles.css $target3
 
 # comentado pq não precisa copiar o repo inteiro
 #cp -r ./ $target
